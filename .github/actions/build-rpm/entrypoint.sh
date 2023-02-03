f2b6b1005332dd80e787663efb51c0151094cdea
Daniel Fulton <dfulton@lbl.gov>
2023-02-02 16:21:40 -0800

    modified build-rpm entrypoint skeleton to produce a file

--- .github/actions/build-rpm/entrypoint.sh
+++ .github/actions/build-rpm/entrypoint.sh
@@ -4,5 +4,5 @@ SRPM=podman-hpc.srpm
 
 echo "Hello $1. I am SRPM!" > $SRPM
 
-echo source_rpm_path=$(pwd)/$PACKAGE >> $GITHUB_OUTPUT
-echo source_rpm_name=$PACKAGE >> $GITHUB_OUTPUT
+echo source_rpm_path=$(pwd)/$SRPM >> $GITHUB_OUTPUT
+echo source_rpm_name=$SRPM >> $GITHUB_OUTPUT
