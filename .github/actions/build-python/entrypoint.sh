9a44d699a3f53e95d69a3a34ec4adbe442cd9cb0
Daniel Fulton <dfulton@lbl.gov>
2023-02-02 20:00:52 -0800

    Attempt to fix SRPM/RPM build.

--- .github/actions/build-python/entrypoint.sh
+++ .github/actions/build-python/entrypoint.sh
@@ -1,8 +1,14 @@
 #!/bin/sh -l
 
+# build python packages
 DIST=dist
-
 python3 -m build --outdir $DIST
 
-echo python_dist=$DIST >> $GITHUB_OUTPUT
+# build SRPM and RPM
+BUILDRPM_ROOT=/usr/src/packages
+cp $DIST/*.tar.gz $(rpmbuild --eval="%{_sourcedir}")/
+rpmbuild -ba podman-hpc.spec
 
+echo python_dist=$DIST >> $GITHUB_OUTPUT
+echo rpm_dir=$BUILDRPM_ROOT/RPMS >> $GITHUB_OUTPUT
+echo srpm_dir=$BUILDRPM_ROOT/SRPMS >> $GITHUB_OUTPUT
