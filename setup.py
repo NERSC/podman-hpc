4fd90a24f1e2635fc2bbc7dc6a8558633da8f4b4
Daniel Fulton <dfulton@lbl.gov>
2026-06-05 10:33:02 -0700

    Monkeypatch to make python3.6 build PEP625 compliant.
    Prior to version 69.3.0 setuptools produces and sdist tarball using
    the canonical python dist name i.e. `podman-hpc` however PEP625
    and PyPA requires the dist name has no hyphens, and the sdist
    filename conform to the same rules as the binary wheel naming
    convention, which is to canonicalize the dist name (all instances
    of [.-_] are replaced by single hypen `-`), and then substitute
    underscores for hyphens.
    
    setuptools >= 69.3.0+ is unavailable in Python 3.6, so this
    fix monkeypatches the canonicalized filename into the build
    process via setup.py when setuptools is at a lower version number.

--- setup.py
+++ setup.py
@@ -2,13 +2,34 @@
 import subprocess
 from setuptools import setup
 from setuptools.command.build_py import build_py
+from setuptools.command.sdist import sdist
+from packaging.version import Version
+try:
+    from importlib.metadata import version
+except ModuleNotFoundError:
+    from importlib_metadata import version
 
+_monkeypatch_pep625 = (Version(version("setuptools")) < Version("69.3.0"))
 
-class BuildMakeCommand (build_py):
+if _monkeypatch_pep625:
+    from types import MethodType
+    from packaging.utils import canonicalize_name, canonicalize_version
+    def _get_fullname_canonicalized(self):
+        return "{}-{}".format(
+            canonicalize_name(self.get_name()).replace('-','_'),
+            canonicalize_version(self.get_version()),
+        )
+
+class build_py_with_make_epilogue(build_py):
     def run(self):
         super(build_py, self).run()
         subprocess.run(['make', 'setuptools-build_py'], check=True)
 
+class sdist_pep625(sdist):
+    def make_distribution(self):
+        if _monkeypatch_pep625:
+            self.distribution.get_fullname = MethodType(_get_fullname_canonicalized, self.distribution)
+        super(sdist, self).make_distribution()
 
 if __name__ == "__main__":
-    setup(cmdclass={'build_py': BuildMakeCommand})
+    setup(cmdclass={'build_py': build_py_with_make_epilogue, 'sdist': sdist_pep625})
